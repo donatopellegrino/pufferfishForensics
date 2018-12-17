27265ff038cdca1bd51084c0797f8b656fcc7472
donatopellegrino <donato.pellegrino@studenti.unitn.it>
2018-12-17 12:28:13 +0100

    finito

--- Fscore/computeMultiFscore.m
+++ Fscore/computeMultiFscore.m
@@ -10,7 +10,10 @@ maybeList = {maybeList.name};
 maybeList = maybeList(3:end);
 [c,d] = size(maybeList);
 
-for i = 1:d
+out = zeros(c,d);
+for i = 1:10
     disp(i);
-    
+    maybeI = double(imread(strcat(maybeMapsPath, "/", maybeList{i})));
+    trueI = double(imread(strcat(trueMapsPath, "/", trueList{i})));
+    out(i) = f_measure(trueI,maybeI);
 end
