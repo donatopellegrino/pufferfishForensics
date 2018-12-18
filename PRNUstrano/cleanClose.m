a3a57a646466e483f65e178dcce9180f75d5d38a
donatopellegrino <donato.pellegrino@studenti.unitn.it>
2018-12-18 16:30:09 +0100

    moooooolto meglio

---
+++ PRNUstrano/cleanClose.m
@@ -0,0 +1,13 @@
+function [outputI,max] = cleanClose(inputI)
+    [blobs, blobsNum] = bwlabel(inputI);
+    max = -1;
+    out = -1;
+    for i = 1:blobsNum
+        temp = sum(blobs(:)==i);
+        if temp > max
+            max = temp;
+            out = i;
+        end
+    end
+    outputI = blobs == out;
+end
